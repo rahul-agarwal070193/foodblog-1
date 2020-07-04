89f3cc6c276b09595edfe20ed518a6e6886db11c
rahul-agarwal070193 <36894631+rahul-agarwal070193@users.noreply.github.com>
2020-07-04 23:33:13 +0530

    Add files via upload

--- foodblog/settings.py
+++ foodblog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -93,6 +94,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
